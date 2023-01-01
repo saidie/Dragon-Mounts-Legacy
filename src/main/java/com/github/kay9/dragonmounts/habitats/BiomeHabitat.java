609460f5c298fac6c70008c0d89d6014fb131be2
Hiroshi Saito <saito.die@gmail.com>
2023-01-02 02:58:43 +0900

    Fix

--- src/main/java/com/github/kay9/dragonmounts/habitats/BiomeHabitat.java
+++ src/main/java/com/github/kay9/dragonmounts/habitats/BiomeHabitat.java
@@ -3,22 +3,23 @@ package com.github.kay9.dragonmounts.habitats;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.HolderSet;
 import net.minecraft.core.Registry;
-import net.minecraft.tags.TagKey;
+import net.minecraft.core.RegistryCodecs;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.biome.Biome;
 
-public record BiomeHabitat(int points, TagKey<Biome> biomeTag) implements Habitat
+public record BiomeHabitat(int points, HolderSet<Biome> biomes) implements Habitat
 {
     public static final Codec<BiomeHabitat> CODEC = RecordCodecBuilder.create(instance -> instance.group(
             Habitat.withPoints(2, BiomeHabitat::points),
-            TagKey.codec(Registry.BIOME_REGISTRY).fieldOf("biome_tag").forGetter(BiomeHabitat::biomeTag)
+            RegistryCodecs.homogeneousList(Registry.BIOME_REGISTRY).fieldOf("biomes").forGetter(BiomeHabitat::biomes)
     ).apply(instance, BiomeHabitat::new));
 
     @Override
     public int getHabitatPoints(Level level, BlockPos pos)
     {
-        return level.getBiome(pos).is(biomeTag)? points : 0;
+        return biomes.contains(level.getBiome(pos))? points : 0;
     }
 
     @Override
--- src/main/java/com/github/kay9/dragonmounts/habitats/FluidHabitat.java
+++ src/main/java/com/github/kay9/dragonmounts/habitats/FluidHabitat.java
@@ -3,23 +3,24 @@ package com.github.kay9.dragonmounts.habitats;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.HolderSet;
 import net.minecraft.core.Registry;
-import net.minecraft.tags.TagKey;
+import net.minecraft.core.RegistryCodecs;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.material.Fluid;
 
-public record FluidHabitat(float multiplier, TagKey<Fluid> fluidType) implements Habitat
+public record FluidHabitat(float multiplier, HolderSet<Fluid> fluids) implements Habitat
 {
     public static final Codec<FluidHabitat> CODEC = RecordCodecBuilder.create(instance -> instance.group(
             Habitat.withMultiplier(0.5f, FluidHabitat::multiplier),
-            TagKey.codec(Registry.FLUID_REGISTRY).fieldOf("fluid_tag").forGetter(FluidHabitat::fluidType)
+            RegistryCodecs.homogeneousList(Registry.FLUID_REGISTRY).fieldOf("fluids").forGetter(FluidHabitat::fluids)
     ).apply(instance, FluidHabitat::new));
 
     @Override
     public int getHabitatPoints(Level level, BlockPos pos)
     {
         return (int) (BlockPos.betweenClosedStream(pos.offset(1, 1, 1), pos.offset(-1, -1, -1))
-                .filter(p -> level.getFluidState(p).is(fluidType))
+                .filter(p -> level.getFluidState(p).is(fluids))
                 .count() * multiplier);
     }
 
